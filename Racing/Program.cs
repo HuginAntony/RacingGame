b7b87221076bab6c8bf4bab59c309c26c14a7cc7
Hugin Antony <HuginAntony@gmail.com>
2021-01-22 15:23:04 +0200

    Handling error input

--- Racing/Program.cs
+++ Racing/Program.cs
@@ -13,35 +13,54 @@ namespace Racing
             var db = new RacingContext();
             var cars = db.Cars.ToList();
             var tracks = db.Tracks.ToList();
+            string input = "";
+            int selectedTrack = 0;
 
             Console.WriteLine("Welcome to the racing game.");
             Console.WriteLine();
-            Console.WriteLine("Please select a racing track by entering the Track Id. Press the Enter key after input.");
-            
-            PrintTrackInfo(tracks);
 
-            var input = Console.ReadLine();
-            var selectedTrack = int.Parse(input);
-
-
-            Console.WriteLine("These are the cars participating in the race:");
-            Console.WriteLine();
-
-            PrintCarInfo(cars);
-
-            var results = new Dictionary<int, int>();
-
-            Console.WriteLine("Press any key to start the race");
-            Console.ReadKey();
-
-            foreach (var car in cars)
+            while (input.ToUpper() != "Q")
             {
-                results.Add(car.Id, car.CalculateTrackScore(tracks.Single(t => t.Id == selectedTrack).TrackPath));
+                Console.Clear();
+                Console.WriteLine(
+                    "Please select a racing track by entering the Track Id. Press the Enter key after input.");
+
+                PrintTrackInfo(tracks);
+
+                input = Console.ReadLine();
+
+                while (!int.TryParse(input, out selectedTrack) && input.ToUpper() != "Q")
+                {
+                    Console.WriteLine("Invalid input. Please enter a Track Id.");
+                    input = Console.ReadLine();
+                }
+
+                if (input.ToUpper() != "Q")
+                {
+                    Console.WriteLine("These are the cars participating in the race:");
+                    Console.WriteLine();
+
+                    PrintCarInfo(cars);
+
+                    var results = new Dictionary<int, int>();
+
+                    Console.WriteLine("Press any key to start the race");
+                    Console.ReadKey();
+
+                    foreach (var car in cars)
+                    {
+                        results.Add(car.Id,
+                            car.CalculateTrackScore(tracks.Single(t => t.Id == selectedTrack).TrackPath));
+                    }
+
+                    OutputResults(results, cars);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Press Q to quit or any key to play again");
+                    input = Console.ReadLine();
+                }
             }
-
-            OutputResults(results, cars);
-
-            Console.ReadKey();
+            Console.WriteLine("Game over!");
         }
 
         private static void OutputResults(Dictionary<int, int> results, List<Car> cars)
