72391ca7cdd4617691bcfeecb2ea855ac7992102
Hugin Antony <HuginAntony@gmail.com>
2021-01-22 14:42:57 +0200

    Adding initial migration

---
+++ Racing.DataAccess/Migrations/20210122124215_DatabaseSetup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Racing.DataAccess.Migrations
+{
+    public partial class DatabaseSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Acceleration = table.Column<int>(type: "int", nullable: false),
+                    Braking = table.Column<int>(type: "int", nullable: false),
+                    CorneringAbility = table.Column<int>(type: "int", nullable: false),
+                    TopSpeed = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Track",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    TrackPath = table.Column<string>(type: "varchar(4000)", unicode: false, maxLength: 4000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Track", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+
+            migrationBuilder.DropTable(
+                name: "Track");
+        }
+    }
+}
