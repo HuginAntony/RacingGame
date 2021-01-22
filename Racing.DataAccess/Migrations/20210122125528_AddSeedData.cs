80afb8328cf2b66131e1be79c72ab525ea28f5ac
Hugin Antony <HuginAntony@gmail.com>
2021-01-22 14:57:31 +0200

    Adding seed data and migration

---
+++ Racing.DataAccess/Migrations/20210122125528_AddSeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Racing.DataAccess.Migrations
+{
+    public partial class AddSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Car",
+                columns: new[] { "Id", "Acceleration", "Braking", "CorneringAbility", "Name", "TopSpeed" },
+                values: new object[,]
+                {
+                    { 1, 8, 3, 4, "CORV", 9 },
+                    { 2, 6, 7, 9, "GTR", 8 },
+                    { 3, 7, 4, 4, "MUSTANG", 9 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Track",
+                columns: new[] { "Id", "Name", "TrackPath" },
+                values: new object[,]
+                {
+                    { 1, "Yas Marina", "11110011100011001110011101" },
+                    { 2, "Daytona", "11111111001111110111100001" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Car",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Car",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Car",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Track",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Track",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
--- Racing.DataAccess/RacingContext.cs
+++ Racing.DataAccess/RacingContext.cs
@@ -37,10 +37,62 @@ namespace Racing.DataAccess
             modelBuilder.ApplyConfiguration(new CarConfiguration());
             modelBuilder.ApplyConfiguration(new TrackConfiguration());
 
+            SeedData(modelBuilder);
             OnModelCreatingPartial(modelBuilder);
         }
 
-     
+        private void SeedData(ModelBuilder modelBuilder)
+        {
+            var cars = new List<Car>
+            {
+                new Car
+                {
+                    Id = 1,
+                    Name = "CORV",
+                    Acceleration = 8,
+                    Braking = 3,
+                    CorneringAbility = 4,
+                    TopSpeed = 9
+                },
+                new Car
+                {
+                    Id = 2,
+                    Name = "GTR",
+                    Acceleration = 6,
+                    Braking = 7,
+                    CorneringAbility = 9,
+                    TopSpeed = 8
+                },
+                new Car
+                {
+                    Id = 3,
+                    Name = "MUSTANG",
+                    Acceleration = 7,
+                    Braking = 4,
+                    CorneringAbility = 4,
+                    TopSpeed = 9
+                },
+            };
+
+            var tracks = new List<Track>
+            {
+                new Track
+                {
+                    Id = 1,
+                    Name = "Yas Marina",
+                    TrackPath = "11110011100011001110011101"
+                },
+                new Track
+                {
+                    Id = 2,
+                    Name = "Daytona",
+                    TrackPath = "11111111001111110111100001"
+                },
+            };
+
+            modelBuilder.Entity<Car>().HasData(cars);
+            modelBuilder.Entity<Track>().HasData(tracks);
+        }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
